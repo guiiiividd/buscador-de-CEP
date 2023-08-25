529866be6d75fe2f526b88950fe07ed1ef8d9992
Guilherme <guilherme@debian.debian.org>
2023-08-25 17:22:24 -0300

    Aplicando Ajustes e Finalizando o Projeto

--- src/js/script.js
+++ src/js/script.js
@@ -4,26 +4,35 @@ const inputBairro = document.getElementById("inputBairro");
 const inputCidade = document.getElementById("inputCidade");
 const inputUF = document.getElementById("inputUF");
 const btnBuscar = document.getElementById("btnBuscar");
+const spanCEPInvalido = document.getElementById("spanCEPInvalido")
 
 btnBuscar.addEventListener('click', (evt)=>{
-    evt.preventDefault();
+  evt.preventDefault();
+  limparCampos();
 
-    let CEP = inputCEP.value.replace("-", "");
-    
-    if(CEP != ""){
-      fetch(`https://brasilapi.com.br/api/cep/v1/${CEP}`)
-      .then(response => response.json())
-      .then(data => {
-          console.log(data)
-          inputEndereco.value = data.street;
-          inputBairro.value = data.neighborhood;
-          inputCidade.value = data.city;
-          inputUF.value = data.state;
-      })
-      .catch(error => {
-          console.error('Erro ao buscar dados da API:', error);
-      });
+  let CEP = inputCEP.value.replace("-", "");
   
-      console.log("Clicou");
-    }
+  if(CEP != ""){
+    fetch(`https://brasilapi.com.br/api/cep/v1/${CEP}`)
+    .then(response => response.json())
+    .then(data => {
+      inputEndereco.value = data.street;
+      inputBairro.value = data.neighborhood;
+      inputCidade.value = data.city;
+      inputUF.value = data.state;
+    })
+    .catch(error => {
+      console.error('Erro ao buscar dados da API:', error);
+    });
+  }else{
+    spanCEPInvalido.style.display = "block"
+  }
 })
+
+function limparCampos(){
+  inputEndereco.value = "";
+  inputBairro.value = "";
+  inputCidade.value = "";
+  inputUF.value = ""; 
+  spanCEPInvalido.style.display = "none"
+}
